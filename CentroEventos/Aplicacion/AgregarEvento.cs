f16cd1a4d4fef8d72ea6019cd9a316e5d18f73e1
Juanarena29 <151331182+Juanarena29@users.noreply.github.com>
2025-05-12 20:04:40 -0300

    Update AgregarEvento.cs

--- CentroEventos/Aplicacion/AgregarEvento.cs
+++ CentroEventos/Aplicacion/AgregarEvento.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace CentroDeportivo;
 
-public class AgregarEvento(IRepositorioEvento repositorio, ValidadorEventos validador)
+public class AgregarEventoUseCase(IRepositorioEvento repositorio, ValidadorEventos validador)
 {
     public void Ejecutar(EventoDeportivo evento) {
         if (!validador.Validar(evento, out string mensajeError)) {
