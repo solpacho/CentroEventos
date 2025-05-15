81ad7fb16006423678f49ae1216ce1a7a2ce4a2a
branrodriguezz <bran3rodriguez@gmail.com>
2025-05-15 00:17:27 -0300

    Update IRepositorioReserva.cs

--- CentroEventos/Aplicacion/IRepositorioReserva.cs
+++ CentroEventos/Aplicacion/IRepositorioReserva.cs
@@ -1,5 +1,17 @@
-using Aplicacion;
+using System;
+using CentroEventos.Aplicacion;
 
-public interface IRepositorioReserva{
- // falta hacer
+namespace CentroEventos.Aplicacion;
+
+public interface IRepositorioReserva
+{
+    void AgregarReserva(Reserva reserva);
+    Reserva? ObtenerPorId(int id);
+    List<Reserva> ListarReservas();
+    void Modificar(Reserva reserva);
+    void Eliminar(int id);
+
+    // Métodos auxiliares
+    bool ReservaExistente(int personaId, int eventoId);
+    int CantidadReservasEvento(int eventoId);
 }
