34c7aaf5640a9b77f22786e41d4e5cefa0e29181
branrodriguezz <bran3rodriguez@gmail.com>
2025-05-16 20:08:10 -0300

    Update IRepositorioEvento.cs

--- CentroEventos/Aplicacion/IRepositorioEvento.cs
+++ CentroEventos/Aplicacion/IRepositorioEvento.cs
@@ -6,15 +6,12 @@ public interface IRepositorioEvento
     //id
     int ObtenerNuevoId();
     //
-
     void AgregarEvento(EventoDeportivo evento);
     List<EventoDeportivo> ListarEventos();
     void EliminarEvento(int id);
-
     bool esResponsable(int id);
-
     bool existeEvento(int id);
-
-    bool controlReserva(EventoDeportivo evento)
+    bool controlReserva(int id);
+    void Modificar(EventoDeportivo evento);
     
 }
