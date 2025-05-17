3290683058e8f04f207cbda56d7ed350111e92c4
clarisunlp <claramsuarez20@gmail.com>
2025-05-16 22:58:43 -0300

    PersonaAltaUseCase.cs

--- CentroEventos/Aplicacion/PersonaAltaUseCase.cs
+++ CentroEventos/Aplicacion/PersonaAltaUseCase.cs
@@ -1,20 +1,27 @@
+using Aplicacion;
+
 namespace CentroEventos.Aplicacion;
 
 public class PersonaAltaUseCase(IRepositorioPersona repositorio, ValidadorPersona validador, IServicioAutorizacion autorizacion){
 
     public void Ejecutar(Persona persona, int idUsuario)
     {
-        if (!validador.Validar(persona, out string mensajeError))
-            throw new ValidacionException(mensajeError);
+        if (!autorizacion.PoseeElPermiso(idUsuario, Permiso.UsuarioAlta)) { //preguntar primero
+            throw new FalloAutorizacionException("No posee permisos para realizar esta acción. \n");
+        }
 
-        if (repositorio.DniRepetido(persona.Dni)) {
+        if (!validador.Validar(persona, out string mensajeError)){
+            throw new ValidacionException(mensajeError);
+        }
+        
+        if (repositorio.DniRepetido(persona.DNI)){
             throw new DuplicadoException("El Dni ya existe. \n");
         }
 
-        if (repositorio.EmailRepetido(persona.Email)) {
+        if (repositorio.EmailRepetido(persona.Email)){
             throw new DuplicadoException("El Email ya existe. \n");
         }
-
+        
         persona.Id = repositorio.ObtenerNuevoId(); //asigno id.
 
         repositorio.AgregarPersona(persona);
