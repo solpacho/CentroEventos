c3aae3c7282d0d914e7f6e156f164cf918794d21
clarisunlp <claramsuarez20@gmail.com>
2025-05-16 22:56:33 -0300

    EventoDeportivoAltaUseCase.cs

--- CentroEventos/Aplicacion/EventoDeportivoAltaUseCase.cs
+++ CentroEventos/Aplicacion/EventoDeportivoAltaUseCase.cs
@@ -1,21 +1,21 @@
 using System;
+using Aplicacion;
 
 namespace CentroEventos.Aplicacion;
 
 public class EventoDeportivoAltaUseCase(IRepositorioEvento repositorio, ValidadorEventos validador, IServicioAutorizacion autorizacion)
 {
     public void Ejecutar(EventoDeportivo evento, int idUsuario) {
-       
-        if (!validador.Validar(evento, out string mensajeError)) {
-            throw new ValidacionException(mensajeError);
-        }
-
-        if (!autorizacion.PoseeElPermiso(idUsuario, Permiso.AgregarEvento)) {
+        
+        if (!autorizacion.PoseeElPermiso(idUsuario, Permiso.EventoAlta)){
             throw new FalloAutorizacionException("No posee permisos para esta acción.");
+        }
+       
+        if (!validador.Validar(evento, out string mensajeError)){
+            throw new ValidacionException(mensajeError);
         }
         
         evento.Id=repositorio.ObtenerNuevoId();
 
         repositorio.AgregarEvento(evento);
-    }
-}
+   
