e309d8b6e21b10ed0b31daffb16e12bb4727f4f0
clarisunlp <claramsuarez20@gmail.com>
2025-05-16 23:27:34 -0300

    ListarAsistenciaAEventoUseCase.cs

--- CentroEventos/Aplicacion/ListarAsistenciaAEventoUseCase.cs
+++ CentroEventos/Aplicacion/ListarAsistenciaAEventoUseCase.cs
@@ -1,4 +1,38 @@
-namespace Aplicacion
-public class ListarAsistenciaAEventoUseCase(IRepositorioEventos repositorio){
-         //falta la generacion de la lista en el repo
+using System;
+using CentroEventos.Aplicacion;
+namespace Aplicacion;
+
+public class ListarAsistenciaAEventoUseCase(IRepositorioEvento repositorioE, IRepositorioReserva repositorioR, IRepositorioPersona repositorioP ValidadorEventos validador)
+{
+    public List<Persona> Ejecutar(EventoDeportivo evento)
+    {
+        List<Persona> listaPresentes = new List<Persona>();
+
+        if (!repositorioE.existeEvento(evento.Id)){
+                 
+            throw new EntidadNotFoundException("No se ha encontrado el evento. \n");
+        }
+
+        if (!validador.Validar(evento, out string mensajeError)){
+                 
+            throw new ValidacionException(mensajeError);
+        }
+
+        if (!(evento.FechaHoraInicio < DateTime.Now)){ //si no es un evento pasado
+                 
+            throw new OperacionInvalidaException("Este evento no es un evento pasado");
+        }
+
+        List<Reserva> reservas = repositorioR.ListarReservas(); //obtengo todas las reservas
+        foreach (Reserva r in reservas)
+        {
+            if ((r.EventoDeportivoId == evento.Id) && (r.Estado == Reserva.EstadoAsistencia.Presente)) //si la reserva es de este evento y se asistió
+            {
+                Persona p = repositorioP.ObtenerPorId(r.PersonaId);
+                listaPresentes.Add(p);
+            }
+        }
+        return listaPresentes;
+        
+    }
 }
