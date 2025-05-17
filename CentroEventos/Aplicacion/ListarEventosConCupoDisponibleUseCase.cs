32d43fbb054a0375a8e8f3cdceb3440225d7d01e
clarisunlp <claramsuarez20@gmail.com>
2025-05-16 23:28:00 -0300

    ListarEventosConCupoDisponibleUseCase.cs

--- CentroEventos/Aplicacion/ListarEventosConCupoDisponibleUseCase.cs
+++ CentroEventos/Aplicacion/ListarEventosConCupoDisponibleUseCase.cs
@@ -1,5 +1,27 @@
-namespace Aplicacion
+using CentroEventos.Aplicacion;
 
-public class ListarEventosConCupoDisponibleUseCase(IRepositorioEventos repositorio){
-    //falta la generacion de la lista en el repo
+namespace Aplicacion;
+
+public class ListarEventosConCupoDisponibleUseCase(IRepositorioEvento repoEventos, IRepositorioReserva repoReserva) {
+    public List<EventoDeportivo> Ejecutar()
+    {
+        List<EventoDeportivo> lista = new List<EventoDeportivo>();
+
+        var listaeventos = repoEventos.ListarEventos();
+
+        foreach (EventoDeportivo evento in listaeventos)
+        {
+            if (evento.FechaHoraInicio >= DateTime.Now)
+            {
+                int reservasevento = repoReserva.CantidadReservasEvento(evento.Id);
+
+                if (evento.CupoMaximo > reservasevento)
+                {
+                    listaeventos.Add(evento);
+                }
+            }
+        }
+
+        return listaeventos;
+    }
 }
