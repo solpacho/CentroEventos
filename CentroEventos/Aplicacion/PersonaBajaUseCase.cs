b4da5d63ad20b5625c094afac51a6fef3ab43c41
clarisunlp <claramsuarez20@gmail.com>
2025-05-16 22:59:22 -0300

    PersonaBajaUseCase.cs

--- CentroEventos/Aplicacion/PersonaBajaUseCase.cs
+++ CentroEventos/Aplicacion/PersonaBajaUseCase.cs
@@ -2,7 +2,7 @@ using CentroEventos.Aplicacion;
 
 namespace Aplicacion;
 
-public class PersonaBajaUseCase(IRepositorioPersona repositorioP, IServicioAutorizacion autorizacion, IRepositorioEventoDeportivo repositorioEV, IRepositorioReserva repositorioR)
+public class PersonaBajaUseCase(IRepositorioPersona repositorioP, IServicioAutorizacion autorizacion, IRepositorioEvento repositorioEV, IRepositorioReserva repositorioR, ValidadorPersona validador)
 {
     public void Ejecutar(int idPersona, int idUsuario)
     {
@@ -10,11 +10,18 @@ public class PersonaBajaUseCase(IRepositorioPersona repositorioP, IServicioAutor
             throw new FalloAutorizacionException("No posee permisos para realizar esta acción. \n");
         }
 
-        if (!repositorioP.existePersona(idPersona)) {
+        if (!repositorioP.existePersona(idPersona))
+        {
             throw new EntidadNotFoundException("No se ha encontrado la persona. \n");
         }
-        // reglas
+
+        Persona p = repositorioP.ObtenerPorId(idPersona); //hago esto unicamente si la persona existe
+        if (!validador.Validar(p, out string mensajeError)){ //no se como resolver la warning
+            throw new ValidacionException(mensajeError);
+        }
         
+        // reglas
+
         if (repositorioEV.esResponsable(idPersona))
         {
             throw new OperacionInvalidaException("No puede eliminarse esta persona.");
