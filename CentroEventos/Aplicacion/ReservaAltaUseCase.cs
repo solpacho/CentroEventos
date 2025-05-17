399ac35ad6b3e3cf0762c34291bf3a21e5a522e2
clarisunlp <claramsuarez20@gmail.com>
2025-05-16 23:00:30 -0300

    ReservaAltaUseCase.cs

--- CentroEventos/Aplicacion/ReservaAltaUseCase.cs
+++ CentroEventos/Aplicacion/ReservaAltaUseCase.cs
@@ -12,22 +12,27 @@ IRepositorioPersona repoPersona, IServicioAutorizacion autorizacion)
         }
 
         if (repoPersona.existePersona(datosReserva.PersonaId)) {
-            throw new EntidadNotFound("No existe persona con ese ID. \n");
+            throw new EntidadNotFoundException("No existe persona con ese ID. \n");
         }
         
         if (repoEvento.existeEvento(datosReserva.EventoDeportivoId)) {
-            throw new EntidadNotFound("No existe evento con ese ID. \n");
+            throw new EntidadNotFoundException("No existe evento con ese ID. \n");
         }
 
         if (repoReserva.ReservaExistente(datosReserva.PersonaId, datosReserva.EventoDeportivoId)) {
-            throw new DuplicadoExcepcion("Ya se ha reservado a esta persona para este evento. \n");
+            throw new DuplicadoException("Ya se ha reservado a esta persona para este evento. \n");
         }
 
         //hacer segun reglas, hagan asi cada if de las excepciones es mas legible
+
+        //Un EventoDeportivo no puede tener más Reservas que su CupoMaximo.
+        EventoDeportivo evento = repoEvento.obtenerEvento(datosReserva.EventoDeportivoId); //faltaria esto en el repo de eventos
+        if (!(repoReserva.CantidadReservasEvento(datosReserva.Id) < evento.CupoMaximo)){
+            throw new CupoExcedidoException("El cupo de reservas de ese evento deportivo está lleno \n");
+        }
 
         //asigno id
         datosReserva.Id=repoReserva.ObtenerNuevoId();
 
         repoReserva.AgregarReserva(datosReserva);
     }
-}
