677e30cbd778d077bc4fcbc79ee139434d619dce
clarisunlp <claramsuarez20@gmail.com>
2025-05-16 22:54:27 -0300

    Add files via upload

---
+++ CentroEventos/EventoDeportivoAltaUseCase.cs
@@ -0,0 +1,22 @@
+using System;
+using Aplicacion;
+
+namespace CentroEventos.Aplicacion;
+
+public class EventoDeportivoAltaUseCase(IRepositorioEvento repositorio, ValidadorEventos validador, IServicioAutorizacion autorizacion)
+{
+    public void Ejecutar(EventoDeportivo evento, int idUsuario) {
+        
+        if (!autorizacion.PoseeElPermiso(idUsuario, Permiso.EventoAlta)){
+            throw new FalloAutorizacionException("No posee permisos para esta acción.");
+        }
+       
+        if (!validador.Validar(evento, out string mensajeError)){
+            throw new ValidacionException(mensajeError);
+        }
+        
+        evento.Id=repositorio.ObtenerNuevoId();
+
+        repositorio.AgregarEvento(evento);
+    }
+}
