fe854ac7c37d8aeabccfb90128dfc0b8566461f7
clarisunlp <claramsuarez20@gmail.com>
2025-05-21 15:35:45 -0300

    ReservaModificacionUseCase.cs

--- CentroEventos/Aplicacion/ReservaModificacionUseCase.cs
+++ CentroEventos/Aplicacion/ReservaModificacionUseCase.cs
@@ -15,6 +15,5 @@ public class ReservaModificacionUseCase(IRepositorioReserva repositorio, IServic
         }
  
         repositorio.Modificar(reserva);
-        Console.WriteLine("Reserva modificada con éxito");
     }
 }
