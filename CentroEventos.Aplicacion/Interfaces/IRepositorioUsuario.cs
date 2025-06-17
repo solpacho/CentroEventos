7ee7289679b1d2c104cf14618fe89688a2024ed4
Juanarena29 <151331182+Juanarena29@users.noreply.github.com>
2025-06-17 03:08:13 -0300

    Update IRepositorioUsuario.cs

--- CentroEventos.Aplicacion/Interfaces/IRepositorioUsuario.cs
+++ CentroEventos.Aplicacion/Interfaces/IRepositorioUsuario.cs
@@ -11,6 +11,7 @@ public interface IRepositorioUsuario
 {
     void AgregarUsuario(Usuario u);
     List<Usuario> ListarUsuarios();
+    bool EmailRepetido(string email);
 
     // implementar void darPermiso
 
