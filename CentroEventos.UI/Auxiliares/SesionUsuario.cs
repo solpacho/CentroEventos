adb66250b45c3a26eb21a40c04abf8ee9d865b43
clarisunlp <claramsuarez20@gmail.com>
2025-06-18 11:47:18 -0300

    SesionUsuario.cs

--- CentroEventos.UI/Auxiliares/SesionUsuario.cs
+++ CentroEventos.UI/Auxiliares/SesionUsuario.cs
@@ -1,16 +1,21 @@
 using CentroEventos.Aplicacion;
+using CentroEventos.Repositorios;
 
-public class SesionUsuario
+//esto lo que hace es controlar el esado de la sesión. Cuando se ejecuta se obtiene el Usuario mediante
+// IniciarSesionUseCase (si devuelve null=sesion no iniciada, si es <> null, sesión iniciada). Obtiene el
+//Usuario pq precisa ver su id para conocer sus permisos. Cuando se llama a CerrarSesion pone Usario en null
+public class SesionUsuario(IniciarSesionUseCase login)
 {
+
     public Usuario? Usuario { get; private set; }
 
     public event Action? OnChange;
 
     public bool EstaLogueado => Usuario != null;
 
-    public void IniciarSesion(Usuario u)
+    public void IniciarSesion(String email, String contrasenia)
     {
-        Usuario = u;
+        Usuario = login.Ejecutar(email, contrasenia);
         OnChange?.Invoke();
     }
 
