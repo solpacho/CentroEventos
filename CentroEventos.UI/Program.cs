8af796d18e5165c517e1c0ed3557822838719b4b
solpacho <sol.pacho17@gmail.com>
2025-06-19 01:30:40 -0300

    Update Program.cs

--- CentroEventos.UI/Program.cs
+++ CentroEventos.UI/Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddScoped<IRepositorioPersona, RepositorioPersonaSQL>(); // fal
 builder.Services.AddScoped<IRepositorioEvento,  RepositorioEventosSQL>(); // faltaba agregar
 builder.Services.AddScoped<IniciarSesionUseCase>();
 builder.Services.AddScoped<SesionUsuario>();
+// UseCase para listar responsables
+builder.Services.AddScoped<ListarResponsablesUseCase>()
 
 var app = builder.Build();
 RepositorioSqlite.Inicializar();
