36162ddb648b0dcbf7328fe2bf17883febf7274f
clarisunlp <claramsuarez20@gmail.com>
2025-06-18 23:06:34 -0300

    Add files via upload

---
+++ CentroEventos.UI/Models/OtorgarPermisoModel.cs
@@ -0,0 +1,8 @@
+namespace CentroEventos.UI;
+using CentroEventos.Aplicacion;
+
+public class OtorgarPermisoModel
+{
+        public int IdReceptor { get; set; }
+        public Permiso Permiso { get; set; }
+}
