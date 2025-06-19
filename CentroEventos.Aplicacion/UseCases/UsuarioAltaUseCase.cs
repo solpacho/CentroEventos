dceb2c4aae090d1799c12570b38b8711937c490e
clarisunlp <claramsuarez20@gmail.com>
2025-06-19 11:09:48 -0300

    UsuarioAltaUseCase.cs

--- CentroEventos.Aplicacion/UseCases/UsuarioAltaUseCase.cs
+++ CentroEventos.Aplicacion/UseCases/UsuarioAltaUseCase.cs
@@ -6,8 +6,14 @@ namespace CentroEventos.Aplicacion;
 public class UsuarioAltaUseCase(IRepositorioUsuario repou, ValidadorUsuario validador, HashHelperUseCase hashHelper)
 {
     public void Ejecutar(Usuario u, string passwordPlano)
-    {   
+    {
+
         // 1. Validación de datos del usuario
+        if (string.IsNullOrEmpty(passwordPlano))
+        {
+            throw new ValidacionException("La contraseña no puede estar vacío. \n");
+        }
+
         if (!validador.Validar(u, out string mensajeError))
         {
             throw new ValidacionException(mensajeError);
