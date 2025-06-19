ab052511dff3884838b717a063be7e01cbccdf01
solpacho <sol.pacho17@gmail.com>
2025-06-19 11:15:23 -0300

    Update UsuarioAltaUseCase.cs

--- CentroEventos.Aplicacion/UseCases/UsuarioAltaUseCase.cs
+++ CentroEventos.Aplicacion/UseCases/UsuarioAltaUseCase.cs
@@ -2,7 +2,6 @@ using CentroEventos.Aplicacion;
 
 namespace CentroEventos.Aplicacion;
 
-// falta agregar AUTORIZACIÓN para poder dar alta usuario
 public class UsuarioAltaUseCase(IRepositorioUsuario repou, ValidadorUsuario validador, HashHelperUseCase hashHelper)
 {
     public void Ejecutar(Usuario u, string passwordPlano)
